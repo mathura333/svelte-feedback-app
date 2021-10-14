94ba5ae217364e32272d13c892bbe40fd1ba4d05
Mathura Das <mathuradas333@gmail.com>
2021-10-15 01:48:56 +0530

    Practice part

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
-	export let name: string;
+	let firstName="Foo";
+	let lastName= "Bar";
+	let color = "blue";
+	let showText = false;
+	let users = [{
+		id:1,
+		name:'Jhon'
+	},{
+		id:2,
+		name:'Sara'
+	},{
+		id:3,
+		name:'Doo'
+	}]
+	$: name = firstName + " " + lastName;
+	const toggle = () => {
+		color = color === 'blue' ? "purple" : "blue"
+		showText= !showText
+		users = [...users, {id:4,name:"new User"}]
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	{#if showText}
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+	<p>Hello Para</p>
+	{/if}
+	<button on:click={toggle}>Change color</button>
+	{#each users as user (user.id)}
+	<h3>{user.name}</h3>
+	{/each}
 </main>
 
 <style>
@@ -20,6 +47,10 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	button{
+		cursor: pointer;
 	}
 
 	@media (min-width: 640px) {
